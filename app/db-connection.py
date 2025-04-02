02e7505eaac62094093cc7fc066542dcf50621b6
kamileg <kamil.kalicki.dev@gmail.com>
2025-04-02 18:38:52 +0200

    added db connection script.

--- app/db-connection.py
+++ app/db-connection.py
@@ -1,5 +1,21 @@
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
+DEV_DATABASE_URL = os.getenv("DEV_DATABASE")
 
+engine = create_engine(DEV_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=True)
+
+Base = declarative_base
+
+
+def get_db_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/hello")
 def hello():
     return {"Hello": "How you doin'?"}
